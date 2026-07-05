5eb2d329a555380bb6fe2f05b97aa08a168854fa
SrushtiMhaske09 <srushtimhaske09@gmail.com>
2026-07-05 14:17:29 +0530

    React setup with vite

---
+++ REACTJS/react project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Guys its me Srushti</h1>
+      <h3>Content Creator</h3>
+      <h3>Turning real life chaos into content</h3>
+      <button>Follow</button>
+    </div>
+  )
+}
+
+export default App
