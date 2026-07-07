bc39907be5797141e9166548850724d4a5b22ac3
SrushtiMhaske09 <srushtimhaske09@gmail.com>
2026-07-08 01:26:44 +0530

    tailwind css implementation

---
+++ REACTJS/Day-4/Tailwind css/src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'> Srushti mhaske</h1>
+      <p className='text-lg text-gray-600'>This is a sample paragraph using Tailwind CSS.</p>
+      <h3 className="bg-blue-400 "> content creator</h3>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Click Me</button>
+    </div>
+  )
+}
+
+export default App
